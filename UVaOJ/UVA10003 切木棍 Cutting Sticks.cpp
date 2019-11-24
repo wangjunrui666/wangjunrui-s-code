685e7fbae10bcad2e29dbefb3e779997def49500
wangjunrui666 <2189535056@qq.com>
2019-11-24 15:35:25 +0800

    update

--- UVaOJ/UVA10003 切木棍 Cutting Sticks.cpp
+++ UVaOJ/UVA10003 切木棍 Cutting Sticks.cpp
@@ -4,68 +4,26 @@
 #include<cstring>
 #define re register
 using namespace std;
-namespace IO
+template <typename T>
+inline void read(T& x)
 {
-	const int SIZE=1<<25;
-	char ibuf[SIZE],*iS,*iT;
-#define gc() (iS==iT?(iT=(iS=ibuf)+fread(ibuf,1,SIZE,stdin),iS==iT?EOF:*iS++):*iS++)
-	template<typename T>
-	inline void read(T & x)
+	x = 0;
+	char s = getchar();
+	bool f = false;
+	while (!(s >= '0' && s <= '9'))
 	{
-		x=0;
-		bool b=false;
-		char ch=gc();
-		while(!isdigit(ch)&&ch^'-')
-			ch=gc();
-		if(ch=='-')
-		{
-			b=true;
-			ch=gc();
-		}
-		while(isdigit(ch))
-		{
-			x=(x<<1)+(x<<3)+(ch^'0');
-			ch=gc();
-		}
-		if(b)
-			x=~x+1;
-		return;
+		if (s == '-')
+			f = true;
+		s = getchar();
 	}
-#undef gc
-	char Out[1<<30],*fe=Out,ch[25];
-	int num=0;
-	template<typename T>
-	inline void write(T x)
+	while (s >= '0' && s <= '9')
 	{
-		if(!x)
-			*fe++='0';
-		if(x<0)
-		{
-			*fe++='-';
-			x=-x;
-		}
-		while(x)
-		{
-			ch[++num]=x%10+'0';
-			x/=10;
-		}
-		while(num)
-			*fe++=ch[num--];
-		*fe++='\n';
+		x = (x << 1) + (x << 3) + s - '0';
+		s = getchar();
 	}
-	inline void write_str(char *s)
-	{
-		for(re int i=0; s[i]; i++)
-			*fe++=s[i];
-		*fe++='\n';
-	}
-	inline void flush()
-	{
-		fwrite(Out,1,fe-Out,stdout);
-		fe=Out;
-	}
+	if (f)
+		x = (~x) + 1;
 }
-using namespace IO;
 const int N=1010,INF=0x3f3f3f3f;
 int l,n,a[N],dp[101][101];
 int main()
--- 洛谷OJ/P3369 【模板】普通平衡树.cpp
+++ 洛谷OJ/P3369 【模板】普通平衡树.cpp
@@ -1,225 +1,171 @@
-#include<iostream>
 #include<cstdio>
 #include<cstdlib>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
-using namespace std;
-#define lson ch[0]
+#include<iostream>
+#include<ctime>
 #define re register
-#define rson ch[1]
-#define MAX 500100
-int root=0,N,tot=0;
-namespace IO
+using namespace std;
+const int INF=2147483647,N=1e5+10;
+int cnt,rt;
+int size[N],val[N],num[N],rk[N],son[N][2];
+template<typename T>
+inline void read(T&x)
 {
-	const int SIZE=1<<30;
-	char ibuf[SIZE],*iS,*iT;
-#define gc() (iS==iT?(iT=(iS=ibuf)+fread(ibuf,1,SIZE,stdin),iS==iT?EOF:*iS++):*iS++)
-	template<typename T>
-	inline void read(T & x)
+	x=0;
+	char s=getchar();
+	bool f=false;
+	while(!(s>='0'&&s<='9'))
 	{
-		x=0;
-		bool b=false;
-		char ch=gc();
-		while(!isdigit(ch)&&ch^'-')
-			ch=gc();
-		if(ch=='-')
-		{
-			b=true;
-			ch=gc();
-		}
-		while(isdigit(ch))
-		{
-			x=(x<<1)+(x<<3)+(ch^'0');
-			ch=gc();
-		}
-		if(b)
-			x=~x+1;
+		if(s=='-')
+			f=true;
+		s=getchar();
+	}
+	while(s>='0'&&s<='9')
+	{
+		x=(x<<1)+(x<<3)+s-'0';
+		s=getchar();
+	}
+	if(f)
+		x=(~x)+1;
+	return;
+}
+inline void pushup(int p)
+{
+	size[p]=size[son[p][0]]+size[son[p][1]]+num[p];
+}
+inline void rotate(int &p,int d)
+{
+	int k=son[p][d^1];
+	son[p][d^1]=son[k][d];
+	son[k][d]=p;
+	pushup(k);
+	pushup(p);
+	p=k;
+}
+inline void ins(int &p,int x)
+{
+	if(!p)
+	{
+		p=++cnt;
+		size[p]=num[p]=1;
+		val[p]=x;
+		rk[p]=rand();
 		return;
 	}
-	char Out[1<<25],*fe=Out,ch[25];
-	int num=0;
-	template<typename T>
-	inline void write(T x)
+	if(val[p]==x)
 	{
-		if(!x)
-			*fe++='0';
-		if(x<0)
-		{
-			*fe++='-';
-			x=-x;
-		}
-		while(x)
-		{
-			ch[++num]=x%10+'0';
-			x/=10;
-		}
-		while(num)
-			*fe++=ch[num--];
-		*fe++='\n';
-	}
-	inline void write_str(char *s)
-	{
-		for(re int i=0; s[i]; i++)
-			*fe++=s[i];
-		*fe++='\n';
-	}
-	inline void flush()
-	{
-		fwrite(Out,1,fe-Out,stdout);
-		fe=Out;
-	}
-}
-using namespace IO;
-struct Node
-{
-	int ch[2],father,cnt,val,son;
-} tree[MAX];
-
-inline void push_up(int u)
-{
-	tree[u].son=tree[tree[u].lson].son+tree[tree[u].rson].son+tree[u].cnt;
-}
-
-inline void rotate(int x)
-{
-	register int y=tree[x].father;
-	register int z=tree[y].father;
-	register int k=tree[y].rson==x;
-	tree[z].ch[tree[z].rson==y]=x;
-	tree[x].father=z;
-	tree[y].ch[k]=tree[x].ch[k^1];
-	tree[tree[x].ch[k^1]].father=y;
-	tree[x].ch[k^1]=y;
-	tree[y].father=x;
-	push_up(y);
-	push_up(x);
-}
-
-inline void Splay(int x,int goal)
-{
-	while(tree[x].father!=goal)
-	{
-		int y=tree[x].father;
-		int z=tree[y].father;
-		if(z!=goal)
-			(tree[y].lson==x)^(tree[z].lson==y)?rotate(x):rotate(y);
-		rotate(x);
-	}
-	if(goal==0)
-		root=x;
-}
-
-inline void insert(int x)
-{
-	int u=root,father=0;
-	while(u&&tree[u].val!=x)
-	{
-		father=u;
-		u=tree[u].ch[x>tree[u].val];
-	}
-	if(u)
-		tree[u].cnt++;
-	else
-	{
-		u=++tot;
-		if(father)
-			tree[father].ch[x>tree[father].val]=u;
-		tree[tot].lson=0;
-		tree[tot].rson=0;
-		tree[tot].father=father;
-		tree[tot].val=x;
-		tree[tot].cnt=1;
-		tree[tot].son=1;
-	}
-	Splay(u,0);
-}
-
-inline void Find(int x)
-{
-	int u=root;
-	if(!u)
+		++num[p];
+		++size[p];
 		return;
-	while(tree[u].ch[x>tree[u].val]&&x!=tree[u].val)
-		u=tree[u].ch[x>tree[u].val];
-	Splay(u,0);
-
-}
-
-inline int Next(int x,int f)
-{
-	Find(x);
-	int u=root;
-	if((tree[u].val>x&&f)||(tree[u].val<x&&!f))
-	return u;
-	u=tree[u].ch[f];
-	while(tree[u].ch[f^1])
-	u=tree[u].ch[f^1];
-	return u;
-}
-
-inline void Delete(int x)
-{
-	int last=Next(x,0);
-	int next=Next(x,1);
-	Splay(last,0);
-	Splay(next,last);
-	int del=tree[next].lson;
-	if(tree[del].cnt>1)
-	{
-		tree[del].cnt--;
-		Splay(del,0);
 	}
+	int d=(x>val[p]);
+	ins(son[p][d],x);
+	if(rk[p]<rk[son[p][d]])
+		rotate(p,d^1);
+	pushup(p);
+}
+inline void del(int &p,int x)
+{
+	if(!p)
+		return;
+	if(x<val[p])
+		del(son[p][0],x);
+	else if(x>val[p])
+		del(son[p][1],x);
 	else
-		tree[next].lson=0;
-}
-
-inline int K_th(int x)
-{
-	int u=root;
-	if(tree[u].son<x)
-		return false;
-	while(true)
 	{
-		int y=tree[u].lson;
-		if(x>tree[y].son+tree[u].cnt)
+		if(!son[p][0]&&!son[p][1])
 		{
-			x-=tree[y].son+tree[u].cnt;
-			u=tree[u].rson;
+			--num[p];
+			--size[p];
+			if(num[p]==0)
+				p=0;
 		}
-		else if(tree[y].son>=x)
-			u=y;
-		else
-			return tree[u].val;
+		else if(son[p][0]&&!son[p][1])
+		{
+			rotate(p,1);
+			del(son[p][1],x);
+		}
+		else if(!son[p][0]&&son[p][1])
+		{
+			rotate(p,0);
+			del(son[p][0],x);
+		}
+		else if(son[p][0]&&son[p][1])
+		{
+			int d=(rk[son[p][0]]>rk[son[p][1]]);
+			rotate(p,d);
+			del(son[p][d],x);
+		}
 	}
+	pushup(p);
+}
+inline int getrank(int p,int x)
+{
+	if(!p)
+		return 0;
+	if(val[p]==x)
+		return size[son[p][0]]+1;
+	if(val[p]>x)
+		return getrank(son[p][0],x);
+	if(val[p]<x)
+		return size[son[p][0]]+num[p]+getrank(son[p][1],x);
+}
+inline int find(int p,int x)
+{
+	if(!p)
+		return 0;
+	if(size[son[p][0]]>=x)
+		return find(son[p][0],x);
+	if(size[son[p][0]]+num[p]<x)
+		return find(son[p][1],x-size[son[p][0]]-num[p]);
+	return val[p];
+}
+inline int pre(int p,int x)
+{
+	if(!p)
+		return -INF;
+	if(val[p]>=x)
+		return pre(son[p][0],x);
+	return max(val[p],pre(son[p][1],x));
+}
+inline int suc(int p,int x)
+{
+	if(!p)
+		return INF;
+	if(val[p]<=x)
+		return suc(son[p][1],x);
+	return min(val[p],suc(son[p][0],x));
 }
 int main()
 {
-	insert(-2147483647);
-	insert(+2147483647);
-	re int N;
-	read(N);
-	re int opt,x;
-	while(N--)
+	srand((unsigned long long)time(0));
+	srand(rand());
+	srand(rand());
+	re int n,opt,x;
+	read(n);
+	while(n--)
 	{
-		read(opt);
-		read(x);
-		if(opt==1)
-			insert(x);
-		else if(opt==2)
-			Delete(x);
-		else if(opt==3)
+		read(opt),read(x);
+		switch(opt)
 		{
-			Find(x);
-			write(tree[tree[root].lson].son);
+			case 1:
+				ins(rt,x);
+				break;
+			case 2:
+				del(rt,x);
+				break;
+			case 3:
+				printf("%d\n",getrank(rt,x));
+				break;
+			case 4:
+				printf("%d\n",find(rt,x));
+				break;
+			case 5:
+				printf("%d\n",pre(rt,x));
+				break;
+			case 6:
+				printf("%d\n",suc(rt,x));
+				break;
 		}
-		else if(opt==4)
-			write(K_th(x+1));
-		else if(opt==5)
-			write(tree[Next(x,0)].val);
-		else if(opt==6)
-			write(tree[Next(x,1)].val);
 	}
-	flush();
-	return 0;
 }
