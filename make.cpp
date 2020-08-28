3e1bd7fb6d2b54d3c011d35a35b744b3f20137ec
wangjunrui666 <2189535056@qq.com>
2020-08-28 11:19:24 +0800

    update 2020/8/28

---
+++ make.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define lowbit(x) ((x)&(-(x)))
+#define re register
+#define ll long long
+#define ull unsigned long long
+#define rep(i,a,b) for(re int i=a;i<=b;++i)
+#define per(i,a,b) for(re int i=a;i>=b;--i)
+template<typename T>
+inline void read(T&x)
+{
+	x=0;
+	char s=(char)getchar();
+	bool f=false;
+	while(!(s>='0'&&s<='9'))
+	{
+		if(s=='-')
+			f=true;
+		s=(char)getchar();
+	}
+	while(s>='0'&&s<='9')
+	{
+		x=(x<<1)+(x<<3)+s-'0';
+		s=(char)getchar();
+	}
+	if(f)
+		x=(~x)+1;
+}
+template<typename T,typename ...T1>
+inline void read(T&x,T1&...x1)
+{
+	read(x);
+	read(x1...);
+}
+template<typename T>
+inline void clear(T*array,int l,int r,int val)
+{
+	memset(&array[l],val,sizeof(T)*(r-l+1));
+}
+using namespace std;
+mt19937 rnd((unsigned int)time(0));
+inline int makerand(int l,int r)
+{
+	return l+rnd()%(r-l+1);
+}
+signed main()
+{
+	return 0;
+}
+
+
